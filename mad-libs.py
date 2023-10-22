f11b64d385f3394feb48b83c5c815c90c538d8e7
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2023-10-22 17:10:54 -0500

    Add variables to hold user input

--- mad-libs.py
+++ mad-libs.py
@@ -1,2 +1,18 @@
 # A Mad Libs Game where the computer prompts the user for words
 # Those words are inserted into blanks to make a fun story
+
+adj1 = input("Adjective: ").lower()
+adj2 = input("Adjective: ").lower()
+type_of_bird = input("Type of Bird: ").lower()
+room_in_house = input("Room in a house: ").lower()
+verb_past_tense1 = input("Verb(past tense): ").lower()
+verb1 = input("Verb: ").lower()
+relative_name = input("Relative's Name: ").title()
+noun1 = input("Noun: ").lower()
+liquid = input("Liquid: ").lower()
+verb_end_w_ing1 = input("Verb ending with -ing: ").lower()
+body_part = input("Part of body (pluarl): ").lower()
+plural_noun = input("Plural noun: ").lower()
+verb_end_w_ing2 = input("Verb ending with -ing: ").lower()
+noun2 = input("Noun: ").lower()
+
